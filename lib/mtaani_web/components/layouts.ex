68f77c017b6dd46ce426197fba8003cbea1061d8
philaturo <philaturo09@gmail.com>
2026-04-01 13:45:19 +0300

    feat(layouts): add Layouts module for template embedding
    - Use MtaaniWeb, :html macro
    - Embed templates from layouts directory
    - Enables root layout with app.html.heex

---
+++ lib/mtaani_web/components/layouts.ex
@@ -0,0 +1,10 @@
+defmodule MtaaniWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  """
+  use MtaaniWeb, :html
+
+  embed_templates "layouts/*"
+end
