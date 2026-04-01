2b14ad2ff62d8112a874bd0a052a8c82cec500a6
philaturo <philaturo09@gmail.com>
2026-04-01 11:23:33 +0300

    refactor: remove legacy Phoenix-generated files Remove all files from the original mtaani_prototype structure that have been replaced by our new modular architecture:
    - Old application files (mtaani_prototype.ex, application.ex, mailer.ex)
    - Old web files (mtaani_prototype_web.ex, controllers, components)
    - Old layout templates (root.html.heex, core_components)
    - Old endpoint, router, and telemetry modules.

--- lib/mtaani_prototype/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule MtaaniPrototype.Mailer do
-  use Swoosh.Mailer, otp_app: :mtaani_prototype
-end
--- lib/mtaani_prototype_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule MtaaniPrototypeWeb.PageController do
-  use MtaaniPrototypeWeb, :controller
-
-  def home(conn, _params) do
-    render(conn, :home)
-  end
-end
