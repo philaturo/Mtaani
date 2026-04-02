cdc69d8055dcde9cb0e29521e937943cf1bbf34f
philaturo <philaturo09@gmail.com>
2026-04-02 18:57:32 +0300

    feat(map): improve map container styling and hook integration
    - Add fixed height container with min-height
    - Remove Place schema dependencies temporarily
    - Add proper map container sizing
    - Prepare for marker integration

--- lib/mtaani_web/live/map_live.ex
+++ lib/mtaani_web/live/map_live.ex
@@ -1,21 +1,34 @@
 defmodule MtaaniWeb.MapLive do
   use MtaaniWeb, :live_view
 
+  # Remove these aliases for now
+  # alias Mtaani.Repo
+  # alias Mtaani.Places.Place
+
   @impl true
   def mount(_params, _session, socket) do
     socket =
       socket
       |> assign(:active_tab, "map")
-       |> assign(:show_emergency, false)
+      |> assign(:show_emergency, false)
       |> assign(:places, [])
       |> assign(:selected_place, nil)
 
     if connected?(socket) do
       Phoenix.PubSub.subscribe(Mtaani.PubSub, "online_count")
+      # Comment out place loading for now
+      # send(self(), :load_places)
     end
 
     {:ok, socket}
   end
+
+  # Comment out the place loading handler
+  # @impl true
+  # def handle_info(:load_places, socket) do
+  #   places = Repo.all(Place) |> Repo.preload(:location)
+  #   {:noreply, push_event(socket, "add_markers", %{places: format_places(places)})}
+  # end
 
   @impl true
   def handle_info({:online_count, count}, socket) do
@@ -25,6 +38,11 @@ defmodule MtaaniWeb.MapLive do
   @impl true
   def handle_event("navigate", %{"page" => page}, socket) do
     {:noreply, push_navigate(socket, to: "/#{page}")}
+  end
+
+  @impl true
+  def handle_event("logout", _, socket) do
+    {:noreply, push_navigate(socket, to: "/logout")}
   end
 
   @impl true
@@ -39,46 +57,30 @@ defmodule MtaaniWeb.MapLive do
     {:noreply, socket}
   end
 
-    # Emergency Modal Handlers
-  @impl true
-  def handle_event("open_emergency", _, socket) do
-    {:noreply, assign(socket, :show_emergency, true)}
-  end
+  # Emergency handlers
+  def handle_event("open_emergency", _, socket), do: {:noreply, assign(socket, :show_emergency, true)}
+  def handle_event("close_emergency", _, socket), do: {:noreply, assign(socket, :show_emergency, false)}
+  def handle_event("call_police", _, socket), do: {:noreply, push_event(socket, "call_number", %{number: "999"})}
+  def handle_event("call_ambulance", _, socket), do: {:noreply, push_event(socket, "call_number", %{number: "911"})}
+  def handle_event("call_contact", %{"phone" => phone}, socket), do: {:noreply, push_event(socket, "call_number", %{number: phone})}
+  def handle_event("share_location", _, socket), do: {:noreply, push_event(socket, "share_location", %{})}
+  def handle_event("sos_alert", _, socket), do: {:noreply, push_event(socket, "sos_alert", %{})}
+  def handle_event("trigger_emergency", _, socket), do: {:noreply, push_event(socket, "trigger_emergency", %{})}
 
-  @impl true
-  def handle_event("close_emergency", _, socket) do
-    {:noreply, assign(socket, :show_emergency, false)}
-  end
-
-  @impl true
-  def handle_event("call_police", _, socket) do
-    {:noreply, push_event(socket, "call_number", %{number: "999"})}
-  end
-
-  @impl true
-  def handle_event("call_ambulance", _, socket) do
-    {:noreply, push_event(socket, "call_number", %{number: "911"})}
-  end
-
-  @impl true
-  def handle_event("call_contact", %{"phone" => phone}, socket) do
-    {:noreply, push_event(socket, "call_number", %{number: phone})}
-  end
-
-  @impl true
-  def handle_event("share_location", _, socket) do
-    {:noreply, push_event(socket, "share_location", %{})}
-  end
-
-  @impl true
-  def handle_event("sos_alert", _, socket) do
-    {:noreply, push_event(socket, "sos_alert", %{})}
-  end
-
-  @impl true
-  def handle_event("trigger_emergency", _, socket) do
-    {:noreply, push_event(socket, "trigger_emergency", %{})}
-  end
+  # Comment out format_places for now
+  # defp format_places(places) do
+  #   Enum.map(places, fn place ->
+  #     %{
+  #       id: place.id,
+  #       name: place.name,
+  #       category: place.category,
+  #       address: place.address,
+  #       location: %{
+  #         coordinates: [place.location.x, place.location.y]
+  #       }
+  #     }
+  #   end)
+  # end
 
   @impl true
   def render(assigns) do
