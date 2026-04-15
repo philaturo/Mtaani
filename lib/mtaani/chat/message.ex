9197199446dba245cd9ae14a60b7324a7b57d120
phaturo <aturophil09@gmail.com>
2026-04-15 22:08:11 +0300

    feat(chat): replace group_id with conversation_id in messages
    - Remove group_id field
    - Add belongs_to conversation association
    - Update changeset to use conversation_id

--- lib/mtaani/chat/message.ex
+++ lib/mtaani/chat/message.ex
@@ -3,28 +3,40 @@ defmodule Mtaani.Chat.Message do
   import Ecto.Changeset
 
   schema "messages" do
-    field :content, :string
-    field :media_url, :string
-    field :media_type, :string  # image, video, audio, document
-    field :media_thumbnail, :string
-    field :is_deleted, :boolean, default: false
-    field :is_edited, :boolean, default: false
-    field :reply_to_id, :id
-    field :delivered_at, :utc_datetime
-    field :read_at, :utc_datetime 
-    
-    # belongs_to automatically creates user_id and group_id fields
-    belongs_to :user, Mtaani.Accounts.User
-    belongs_to :group, Mtaani.Chat.Group
-    
+    field(:content, :string)
+    field(:media_url, :string)
+    field(:media_type, :string)
+    field(:media_thumbnail, :string)
+    field(:is_deleted, :boolean, default: false)
+    field(:is_edited, :boolean, default: false)
+    field(:reply_to_id, :id)
+    field(:delivered_at, :utc_datetime)
+    field(:read_at, :utc_datetime)
+    field(:reactions, :map, default: %{})
+
+    belongs_to(:user, Mtaani.Accounts.User)
+    belongs_to(:conversation, Mtaani.Chat.Conversation)
+
     timestamps()
   end
 
   def changeset(message, attrs) do
     message
-    |> cast(attrs, [:content, :media_url, :media_type, :media_thumbnail, 
-                    :is_deleted, :is_edited, :reply_to_id, :user_id, :group_id])
-    |> validate_required([:user_id, :group_id])
+    |> cast(attrs, [
+      :content,
+      :media_url,
+      :media_type,
+      :media_thumbnail,
+      :is_deleted,
+      :is_edited,
+      :reply_to_id,
+      :user_id,
+      :conversation_id,
+      :delivered_at,
+      :read_at,
+      :reactions
+    ])
+    |> validate_required([:user_id, :conversation_id])
     |> validate_length(:content, max: 2000)
   end
 end
