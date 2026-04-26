eea6c984c8dd0a9bf846589cbca1c13ad42048f7
phaturo <aturophil09@gmail.com>
2026-04-26 14:22:14 +0300

    refactor(places): - Change location field type from geometry to map to avoid Postgrex type errors

--- lib/mtaani/places/place.ex
+++ lib/mtaani/places/place.ex
@@ -1,24 +1,34 @@
 defmodule Mtaani.Places.Place do
   use Ecto.Schema
   import Ecto.Changeset
+  import Geo.PostGIS
 
   schema "places" do
-    field :name, :string
-    field :category, :string
-    field :description, :string
-    field :address, :string
-    field :phone, :string
-    field :price_range, :string
-    field :verified, :boolean, default: false
-    field :location, :map 
-    
+    field(:name, :string)
+    field(:category, :string)
+    field(:description, :string)
+    field(:address, :string)
+    field(:phone, :string)
+    field(:price_range, :string)
+    field(:verified, :boolean, default: false)
+    # Changed from :geometry to "map
+    field(:location, :map)
+
     timestamps()
   end
 
   def changeset(place, attrs) do
     place
-    |> cast(attrs, [:name, :category, :description, :address, :phone, 
-                    :price_range, :verified, :location])
-    |> validate_required([:name, :category, :location])
+    |> cast(attrs, [
+      :name,
+      :category,
+      :description,
+      :address,
+      :phone,
+      :price_range,
+      :verified,
+      :location
+    ])
+    |> validate_required([:name, :category])
   end
 end
